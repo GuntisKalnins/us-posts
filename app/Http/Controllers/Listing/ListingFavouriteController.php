9a994d751a92c96a89ce77602b7a29c874c81de9
Guntis <guntis.kalnins92@gmail.com>
2020-04-01 07:05:23 +0300

    added delete favourites

--- app/Http/Controllers/Listing/ListingFavouriteController.php
+++ app/Http/Controllers/Listing/ListingFavouriteController.php
@@ -13,6 +13,7 @@ class ListingFavouriteController extends Controller
         $this->middleware(['auth']);
     }
 
+
     public function index(Request $request)
     {
         $listings = $request->user()->favouriteListings()->with(['user'])->paginate(10);
@@ -21,11 +22,17 @@ class ListingFavouriteController extends Controller
     }
 
 
-
-
     public function store(Request $request, Area $area, Listing $listing)
     {
         $request->user()->favouriteListings()->syncWithoutDetaching([$listing->id]);
+
+        return back();
+    }
+
+
+    public function destroy(Request $request, Area $area, Listing $listing)
+    {
+        $request->user()->favouriteListings()->detach($listing);
 
         return back();
     }
