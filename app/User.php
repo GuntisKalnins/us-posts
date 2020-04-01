0c1423d349e35a2b9170ab5cb59be8f81b4ce259
Guntis <guntis.kalnins92@gmail.com>
2020-04-01 06:07:22 +0300

    added favourites front

--- app/User.php
+++ app/User.php
@@ -39,8 +39,7 @@ class User extends Authenticatable
 
     public function favouriteListings()
     {
-        return $this->morphedByMany(Listing::class, 'favoritable')
-            ->withPivot(['created_at'])
-            ->orderByPivot('created_at', 'desc');
+        return $this->morphedByMany(Listing::class, 'favouriteable')
+            ->withPivot(['created_at']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,12 @@ Route::group(['prefix' => '/{area}'], function () {
 
     });
 
+    Route::group(['prefix' => '/listing', 'namespace' => 'Listing'], function () {
+        Route::post('/{listing}/favourites', 'ListingFavouriteController@store')->name('listings.favourites.store');
+    });
+
+
+
     Route::get('/{listing}', 'Listing\ListingController@show')->name('listings.show');
 
 
