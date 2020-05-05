6c882f0e1c0bbc9253639ec9df159c2049e7d29e
Guntis <guntis.kalnins92@gmail.com>
2020-05-05 07:26:20 +0300

    little updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,6 @@
             
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
         <script>
             $(document).ready(function() {
