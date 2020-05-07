b2344cac765171e1487ec280d97a710de619d2ab
Guntis <guntis.kalnins92@gmail.com>
2020-05-07 06:27:25 +0300

    nav menu sorting

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,52 +1,47 @@
 <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark shadow-sm">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="/storage/uploads/logo.png" width="45" alt="brand-logo" class="d-inline-block align-middle">
-            <span class="d-inline-block align-middle">US Posts ({{ $area->name }})</span>        
-        </a>
-        
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="/storage/uploads/logo.png" width="45" alt="brand-logo" class="d-inline-block align-middle">
+        <span class="d-inline-block align-middle">US Posts ({{ $area->name }})</span>        
+    </a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
-        <a class="mr-3 d-inline-block" href="http://uk-posts.com">UK Posts</a>
-            <form class="d-inline-block align-middle align-self-left mt-2" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                <input type="hidden" name="cmd" value="_s-xclick" />
-                <input type="hidden" name="hosted_button_id" value="A2NXP8YEXG6EL" />
-                <input  type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-            </form>
+    <form class="d-inline-block align-middle align-self-left mt-2" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="A2NXP8YEXG6EL" />
+        <input  type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    </form>
         <ul class="navbar-nav mr-auto">
+            <li class="nav-item active"><a class="nav-link" href="http://uk-posts.com">UK Posts</a></li>
             <li class="nav-item active"><a class="nav-link" href="{{ route('category.index', [$area]) }}">Categories</a></li>
         </ul>
-            
         
-        <!-- Right Side Of Navbar -->
+    <!-- Right Side Of Navbar -->
         <ul class="navbar-nav ml-auto">
     <!-- Authentication Links -->
-    @guest
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-        </li>
-        @if (Route::has('register'))
+        @guest
             <li class="nav-item">
-        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
-        @endif
-        @else
-        <li class="nav-item active"><a class="nav-link" href="{{ route('listings.create', [$area]) }}">New listing</a></li>
-        <li class="nav-item active"><a class="nav-link" href="{{ route('listings.viewed.index', [$area]) }}">Recently viewed</a></li>
-        <li class="nav-item active"><a class="nav-link" href="{{ route('listings.favourites.index', [$area]) }}">Favourites</a></li>
-        <li class="nav-item active"><a class="nav-link" href="{{ route('listings.published.index', [$area]) }}">My listings</a></li>
-
-
-
-        <li class="nav-item active dropdown">
+            @if (Route::has('register'))
+                <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+            @else
+            <li class="nav-item active"><a class="nav-link" href="{{ route('listings.create', [$area]) }}">New listing</a></li>
+            <li class="nav-item active"><a class="nav-link" href="{{ route('listings.favourites.index', [$area]) }}">Favourites</a></li>
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->name }} <span class="caret"></span>
             </a>
-              
+            
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <li class="nav-item active"><a class="dropdown-item" href="{{ route('listings.viewed.index', [$area]) }}">Recently viewed</a></li>
+            <li class="nav-item active"><a class="dropdown-item" href="{{ route('listings.published.index', [$area]) }}">My listings</a></li>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
@@ -57,8 +52,8 @@
                     @csrf
                 </form>
             </div>
-        </li>
-    @endguest
+            </li>
+        @endguest
             
         </ul>
     </div>
