e01992e83c1c48daf4d49f1d5600ab0b01b764e3
Guntis <guntis.kalnins92@gmail.com>
2020-05-16 13:25:40 +0300

    reomved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,15 +26,6 @@
                 $(document).ready(function() { $(".e1").select2(); });
             });
         </script>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166857449-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-166857449-1');
-        </script>
         
         @yield('scripts')
 
