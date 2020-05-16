34102a938b73af17b6c4f1245fa12ea10b35bfb3
Guntis <guntis.kalnins92@gmail.com>
2020-05-16 13:28:08 +0300

    zb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,15 @@
                 $(document).ready(function() { $(".e1").select2(); });
             });
         </script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166857449-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-166857449-1');
+        </script>
         
         @yield('scripts')
 
